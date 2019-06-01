4bc247c84419fd432473cabfeaf4f4a15e43adeb
Dror Dim <dror.dim@honeybook.com>
2019-06-02 02:14:48 +0300

    remove logs

--- lib/java_buildpack/framework/snyk_demo.rb
+++ lib/java_buildpack/framework/snyk_demo.rb
@@ -35,16 +35,12 @@ module JavaBuildpack
         @logger = JavaBuildpack::Logging::LoggerFactory.instance.get_logger SnykDemo
         vulns = []
 
-        i = 0
         all_jars = (@application.root + '**/*.jar').glob
         n_failures = 0
         response = nil
         all_jars.each do |jar|
           checksum = Digest::SHA1.file jar.to_s
-          puts "==================================="
-          puts "DIM#{i += 1} #{checksum}"
           url = "http://search.maven.org/solrsearch/select?q=1:#{checksum}&wt=json&rows=20"
-          puts "DIM#{i += 1} #{url}"
           begin
             response = Net::HTTP.get(URI(url))
           rescue => e
@@ -54,20 +50,15 @@ module JavaBuildpack
               raise "SnykDemo: too many maven query failures. Stopping process"
             end
           end
-          puts "DIM#{i += 1} #{response}"
           resp = JSON.parse(response, { symbolize_names: true })
-          puts "DIM#{i += 1} #{resp}"
           data = resp[:response]
-          puts "DIM#{i += 1} #{data}"
 
           data[:docs].each do |doc|
-            puts "DIM#{i += 1} #{doc}"
             group_id = doc[:g]
             artifact_id = doc[:a]
             version = doc[:v]
             url = "https://blooming-earth-53687.herokuapp.com/query?groupId=#{group_id}&artifactId=#{artifact_id}&version=#{version}"
             @logger.info "querying vulnerabilities for jar #{group_id}, #{artifact_id}, #{version}"
-            puts "DIM#{i += 1} #{url}"
             begin
               response = Net::HTTP.get(URI(url))
             rescue => e
@@ -80,14 +71,10 @@ module JavaBuildpack
 
             if response != "OK"
               resp = JSON.parse(response, { symbolize_names: true })
-              puts "DIM#{i += 1} #{resp}"
               vulns << [{ group_id: group_id, artifact_id: artifact_id, version: version, cve: resp[:cve] }]
             end
           end
-
-
         end
-
 
         if vulns.length > 0
           @logger.error "Found #{vulns.length} vulnerable packages"
@@ -98,17 +85,12 @@ module JavaBuildpack
         else
           @logger.info "Found 0 vulnerable packages"
         end
-
-        raise "FAIL ME"
       end
 
       # (see JavaBuildpack::Component::BaseComponent#release)
       def release
-
       end
-
-      private
-
+      
     end
   end
 end
